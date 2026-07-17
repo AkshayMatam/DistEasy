1d379b3c60258d054187db90afe21158a68418f4
Nikhil <25nikhil0707@gmail.com>
2026-07-17 20:10:47 +0530

    Connect React frontend with FastAPI backend

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routers.product_router import router as product_router
 
 app = FastAPI(title="DistEasy API")
 
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
@@ -10,12 +22,10 @@ def home():
         "message": "Welcome to DistEasy Backend"
     }
 
-
 @app.get("/health")
 def health():
     return {
         "status": "Running"
     }
-
 
 app.include_router(product_router)
